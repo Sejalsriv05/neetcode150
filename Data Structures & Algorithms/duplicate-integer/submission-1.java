11c101d5c9d9bcd7da0cd47f5b0d4ffb6cfcb8f6
Sejalsriv05 <168273508+Sejalsriv05@users.noreply.github.com>
2026-08-23 02:46:04 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        /**for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;**/
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+
+        }
+        for(int count:map.values()){
+            if(count>1){
+                return true;
+            }
+        }
+        return false;
+
+
+    }      
+    
+}
